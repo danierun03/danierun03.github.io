2dcd807890c67d2c96c9968cee7b11bfcee48f56
danierun03 <danieljoybaguio@gmail.com>
2026-04-06 00:59:37 +0800

    more socials in about section, changed footer to include more socials, more adjustments,

--- script.js
+++ script.js
@@ -12,3 +12,24 @@ function mainMenu() {
         hamImg.style.backgroundColor = "rgb(106, 159, 219)";
     }
 }
+
+const socialAliasList = document.getElementById("social-alias-list")
+function showSocialsMenu() {
+    socialAliasList.classList.toggle("display");
+}
+
+const socialAliasMenuListItems = document.getElementById('social-alias-list').querySelectorAll('li');
+const socialLinksList = document.getElementById('socials-list');
+const socialLinksListItems = socialLinksList.querySelectorAll('ul');
+
+
+socialAliasMenuListItems.forEach((item, index) => {
+    item.addEventListener('click', (event) => {
+        document.getElementById("social-alias").textContent = event.target.textContent
+        for(var i = 0; i < socialLinksListItems.length; i++){
+            socialLinksListItems[i].style.display = "none";
+        }
+        socialLinksListItems[index].style.display = "flex"
+        socialAliasList.classList.toggle("display");
+    })
+})
